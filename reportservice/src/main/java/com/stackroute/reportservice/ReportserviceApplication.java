4529245454dfb49af341c3abe4f70b1342efc357
akhila <bakhila36@gmail.com>
2019-09-12 12:38:05 +0530

    zuul, eureka and config server is implemented

--- reportservice/src/main/java/com/stackroute/reportservice/ReportserviceApplication.java
+++ reportservice/src/main/java/com/stackroute/reportservice/ReportserviceApplication.java
@@ -2,8 +2,10 @@ package com.stackroute.reportservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ReportserviceApplication {
 
 	public static void main(String[] args) {
