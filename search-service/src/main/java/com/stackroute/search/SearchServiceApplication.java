fc0b5c10fd661956900665a57a354619e734dd97
isbaska <isbaska1@in.ibm.com>
2019-09-16 09:56:47 +0530

    issues resolved

--- search-service/src/main/java/com/stackroute/search/SearchServiceApplication.java
+++ search-service/src/main/java/com/stackroute/search/SearchServiceApplication.java
@@ -2,8 +2,10 @@ package com.stackroute.search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SearchServiceApplication {
 
 	public static void main(String[] args) {
